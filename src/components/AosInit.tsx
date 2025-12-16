d7e1e71b315f9f1bea0b7ede95342405f5d836e6
boeckMt <bbmatdd@gmail.com>
2025-12-16 21:26:09 +0100

    fix: load aos css clientside so it is not load if no js

--- src/components/AosInit.tsx
+++ src/components/AosInit.tsx
@@ -1,10 +1,16 @@
 import { component$, useVisibleTask$ } from '@builder.io/qwik';
 import AOS from 'aos';
-import 'aos/dist/aos.css';
+// import 'aos/dist/aos.css';
 
 export const AosInit = component$(() => {
     // useVisibleTask$ ->  Consider using useTask$(), useOn(), useOnDocument(), or useOnWindow()
     useVisibleTask$(() => {
+
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = '/silvana-y-mathias/aos@3.0.0-beta.6/aos.css'; // <- use your correct absolute/base path
+        document.head.appendChild(link);
+        
         AOS.init({
             duration: 800,
             easing: 'ease-out',
